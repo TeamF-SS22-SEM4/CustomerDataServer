d329f2ee094fd3d84aaf4db11bd9c4875316211d
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-03-15 18:04:33 +0100

    added default config

--- src/main/java/at/fhv/ss22/ea/f/customerDbService/util/Config.java
+++ src/main/java/at/fhv/ss22/ea/f/customerDbService/util/Config.java
@@ -1,24 +1,35 @@
 package at.fhv.ss22.ea.f.customerDbService.util;
 
 import java.io.FileInputStream;
+import java.util.Optional;
 import java.util.Properties;
 
 public class Config {
 
     private static final String CONFIG_PATH = "./customerServer.conf";
-    private static Properties props;
+    private static final String DEFAULT_CONFIG_PATH = "./customerServer.default.conf";
+    private static final Properties CURRENT_PROPS;
+    private static final Properties DEFAULT_PROPS;
 
     static {
-        props = new Properties();
-        try (FileInputStream fis = new FileInputStream(CONFIG_PATH)) {
-            props.load(fis);
+        DEFAULT_PROPS = new Properties();
+        try (FileInputStream fis = new FileInputStream(DEFAULT_CONFIG_PATH)) {
+            DEFAULT_PROPS.load(fis);
         } catch (Exception e) {
+            System.err.println("Failed to load default configuration");
             e.printStackTrace();
         }
-    }
-    //TODO maybe, implement default Values
 
+        CURRENT_PROPS = new Properties();
+        try (FileInputStream fis = new FileInputStream(CONFIG_PATH)) {
+            CURRENT_PROPS.load(fis);
+        } catch (Exception e) {
+            //choose to ignore it because we have default configuration
+        }
+    }
+
+    //TODO maybe, implement default Values
     public static String getProperty(String key) {
-        return props.getProperty(key);
+        return Optional.ofNullable(CURRENT_PROPS.getProperty(key)).orElse(DEFAULT_PROPS.getProperty(key));
     }
 }
