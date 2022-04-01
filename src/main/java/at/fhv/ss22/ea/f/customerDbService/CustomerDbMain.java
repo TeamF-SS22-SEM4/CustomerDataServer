0f58ac4116f53b46fba4a3f7f4dcb836d4db19fb
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-04-01 18:19:12 +0200

    finished rmi-stuff and dummy data

--- src/main/java/at/fhv/ss22/ea/f/customerDbService/CustomerDbMain.java
+++ src/main/java/at/fhv/ss22/ea/f/customerDbService/CustomerDbMain.java
@@ -7,16 +7,5 @@ public class CustomerDbMain {
     public static void main(String[] args) {
         RMIServer server = new RMIServer();
         server.start();
-
-        // TODO for me
-        // - remove _id from raw data - DONE
-        // - add ALL data to mongo-init.sh
-        // - adjust SETUP.md - DONE
-        // - .conf files - DONE
-        // - .env.dist.file - DONE
-        // - RMI-stuff - DONE
-        // - mongo testing
-
     }
-
 }
