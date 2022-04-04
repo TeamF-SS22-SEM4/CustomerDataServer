b4d174d45df670f8ba540d94d1cfacb642b624eb
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-04-04 14:18:45 +0200

    removed authentication

--- mongo-init.sh
+++ mongo-init.sh
@@ -3,20 +3,6 @@ set -e
 
 mongo <<EOF
 use ${MONGO_INITDB_DATABASE}
-
-db.createUser({
-  user: '${MONGO_USERNAME}',
-  pwd: '${MONGO_PASSWORD}',
-  roles: [{
-    role: 'readWrite',
-    db: '${MONGO_INITDB_DATABASE}'
-  }]
-});
-
-EOF
-
-mongo <<EOF
-use ${MONGO_INITDB_DATABASE}
 db.createCollection('customers')
 
 EOF
