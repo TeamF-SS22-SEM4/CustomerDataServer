64f8ca324e19e7ab76568ecfcbab710cd492ddd5
Smightym8 <michaelspiegel@live.at>
2022-05-27 19:46:31 +0200

    Worked on kubernetes

--- run-k8-local.sh
+++ run-k8-local.sh
@@ -1,10 +1,15 @@
 eval $(minikube docker-env)
 
 kubectl delete -f ./kubernetes/customer-db-service-deployment.yml
-kubectl delete deployments customer-db-service
-kubectl delete service customer-db-service
+kubectl delete -f ./kubernetes/mongo-db-deployment.yml
+kubectl delete -f ./kubernetes/user-ldap-server-deployment.yml
+kubectl delete -f ./kubernetes/mongo-data-provider.yml
 
 ./gradlew build
 docker build -f Dockerfile -t team-f-customer-data-server .
+docker build -f ./mongo-data-provider/Dockerfile -t team-f-mongo-data-provider .
+
 kubectl apply -f ./kubernetes/customer-db-service-deployment.yml
-kubectl expose deployment customer-db-service --type=LoadBalancer --name=customer-db-service
+kubectl apply -f ./kubernetes/mongo-db-deployment.yml
+kubectl apply -f ./kubernetes/user-ldap-server-deployment.yml
+kubectl apply -f ./kubernetes/mongo-data-provider.yml
